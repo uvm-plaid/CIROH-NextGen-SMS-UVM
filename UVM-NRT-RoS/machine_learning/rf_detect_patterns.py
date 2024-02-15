42100445bc16bff4ab2341c9d991c37fe07516ae
spencerkarofsky <spencerkarofsky@gmail.com>
2024-02-15 14:02:44 -0500

    optimized code

--- UVM-NRT-RoS/machine_learning/rf_detect_patterns.py
+++ UVM-NRT-RoS/machine_learning/rf_detect_patterns.py
@@ -3,12 +3,11 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 df = pd.read_csv("rf.csv")
-print(df.head())
 
 accuracies_by_trees = []
 accuracies_by_n_mfccs = []
 
-for n_mfcc_value in range(1, 20):
+for n_mfcc_value in range(1, 15):
     mean_accuracy = df[df["n_trees"] == n_mfcc_value]["accuracy"].mean()
     accuracies_by_trees.append(mean_accuracy)
 
@@ -19,10 +18,18 @@ for n_mfcc_value in range(1, 20):
 print("Variance in Average Accuracy by Num RF Tree's",np.var(np.array(accuracies_by_trees)))
 print("Variance in Average Accuracy by Num MFCC's",np.var(np.array(accuracies_by_n_mfccs)))
 
+x_mfcc = [x for x in range(1,20)]
+x_tree = [x for x in range(1,15)]
+
+# find log regression
+mfcc_fit = np.polyfit(np.log(x_mfcc), accuracies_by_n_mfccs, 1)
+print(mfcc_fit)
+
 
 fig, axs = plt.subplots(2, 1, layout='constrained')
 axs[0].plot(accuracies_by_n_mfccs)
 axs[1].plot(accuracies_by_trees)
+
 axs[0].set_title("Average Accuracy by Number of MFCC's")
-axs[1].set_title("Average Acccuracy by Number of RF Trees")
+axs[1].set_title("Average Accuracy by Number of RF Trees")
 plt.show()
