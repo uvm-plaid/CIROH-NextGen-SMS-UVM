a0b500be6882bc0ea34651f62b311e419fdcf40d
spencerkarofsky <spencerkarofsky@gmail.com>
2024-02-16 10:10:28 -0500

    completed xgboost.

--- UVM-NRT-RoS/machine_learning/xgb_detect_patterns.py
+++ UVM-NRT-RoS/machine_learning/xgb_detect_patterns.py
@@ -23,11 +23,12 @@ for lr in np.arange(0.1,0.6,0.1):
 print("Variance in Average Accuracy by Num Estimators",np.var(np.array(accuracies_by_n_est)))
 print("Variance in Average Accuracy by Learning Rate",np.var(np.array(accuracies_by_lr)))
 
-
+lr_x_ticks = [0.1, 0.2, 0.3, 0.4, 0.5]
 fig, axs = plt.subplots(2, 1, layout='constrained')
 plt.suptitle("XGBoost Model Evaluation")
 axs[0].plot(accuracies_by_n_est)
-axs[1].plot(accuracies_by_lr)
+axs[1].plot(lr_x_ticks,accuracies_by_lr)
+axs[1].set_xticks([0.1, 0.2, 0.3, 0.4, 0.5])
 
 axs[0].set_title("Average Accuracy by Num Estimators")
 axs[1].set_title("Average Accuracy by Learning Rate")
