baa307eaf968b148d24bf5564d70e8018f8fc308
spencerkarofsky <105813301+spencerkarofsky@users.noreply.github.com>
2024-04-01 08:47:31 -0400

    Update mfcc_extraction.ino
    Microphone records audio samples at approx 6600-6900Hz, then windows them and stores in a 2d array

--- UVM-NRT-RoS/microphone-sampling/mfcc_extraction.ino
+++ UVM-NRT-RoS/microphone-sampling/mfcc_extraction.ino
@@ -20,9 +20,9 @@ int index;
 const int WINDOW_LENGTH = 30; // 30 ms
 void loop() {
 
-  /*
+  /**********
   STEP 1: Calculate Raw Amplitudes from 1 second (1000ms) audio samples
-  */
+  **********/
   // Start timer
   double t0 = millis();
   int amplitude = -1;
@@ -40,14 +40,35 @@ void loop() {
   // Print results
   Serial.print("Time to fill ");
   Serial.print(index);
-  Serial.print("-length array: ");
-  Serial.print(millis()-t0);
-  Serial.println(" ms");
-  /*
+  Serial.print("-length array (ms): ");
+  Serial.println(millis()-t0);
+  Serial.print("Sample Rate (Hz): ");
+  Serial.println(index);
+  /**********
   STEP 2: Windowing
-    Split frames into 30 ms audio "windows" with 50% overlap
-  */
-  
-  
-
+    Split frames into 30 ms audio "windows" with 50% overlap of each window
+  **********/
+  // 2a): Calculate number of windows needed (approximately 410-470 windows) and record index positions of these windows
+  int n_windows = 1;
+  int beg_index = 0;
+  int beg_indexes[470];
+  int index_pos;
+  while(beg_index <= index-(WINDOW_LENGTH*(index/1000.0))){
+    index_pos = n_windows-1;
+    beg_indexes[index_pos] = beg_index;
+    beg_index += (WINDOW_LENGTH*(index/1000.0))/2;
+    ++n_windows;
+  }
+  Serial.print(n_windows);
+  Serial.println(" windows");
+  // 2b) Create 2d-array, windows, where rows are each window, and cols are amplitude values for windows
+  int ROWS = n_windows;
+  int COLS = WINDOW_LENGTH * (index/1000.0);
+  int windows[ROWS][COLS];
+  for (int row = 0; row < ROWS; ++row){
+    for(int col = 0; col < COLS; ++col){
+      int amplitude_value = amplitudes[beg_indexes[row]+col];
+      windows[row][col] = amplitude_value;
+    }
+  }
 }
