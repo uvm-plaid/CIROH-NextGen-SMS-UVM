2dbbb8c9782312f438b8a62aad65c1b7ac19e6cf
Jordan Bourdeau <jordan.bourdeau@uvm.edu>
2024-06-23 15:46:35 -0400

    Some more modularization. Working on getting unit testing to work.

---
+++ UVM-NRT-RoS/WifiBee-SWARM/arduino_code/src/checksum.cpp
@@ -0,0 +1,27 @@
+/**
+ * checksum.cpp
+ * 
+ * File containing function definitions for performing checksum calculations.
+ * 
+ * Created as part of the UVM CIROH project.
+ * 
+ * Author: Jordan Bourdeau
+ * Date Created: 6/16/24 
+*/
+
+#include "checksum.h"
+
+uint8_t nmeaChecksum(const char *sz, size_t len) {
+    size_t i = 0;
+    uint8_t cs;
+
+    if (sz [0] == '$'){
+        i++;
+    }
+
+    for (cs = 0; (i < len) && sz [i]; i++){
+        cs ^= ((uint8_t) sz [i]);
+    }
+
+    return cs;
+}
