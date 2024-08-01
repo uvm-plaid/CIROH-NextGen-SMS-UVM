d9da676895543e7de34d6383e3fb93825e5b7f39
Soheyl Faghir Hagh <52649117+Soheyl77@users.noreply.github.com>
2024-08-01 11:30:27 -0400

    Add files via upload

---
+++ UVM-NRT-RoS/Soheyl_Codes/LoRa_RX.ino
@@ -0,0 +1,59 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
+#define LED 13 // Built-in LED
+#define RF95_FREQ 915.0
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup() {
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.println("Feather LoRa RX Test!");
+
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  if (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  rf95.setTxPower(23, false);
+}
+
+void loop() {
+  if (rf95.available()) {
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+
+    if (rf95.recv(buf, &len)) {
+      Serial.print("Received: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+      // Blink LED to indicate data received
+      digitalWrite(LED, HIGH);
+      delay(200);
+      digitalWrite(LED, LOW);
+    } 
+    else {
+      Serial.println("Receive failed");
+    }
+  }
+}
