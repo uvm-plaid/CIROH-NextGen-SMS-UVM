25c42b892557a35f10bfc74a318ce846895cf647
sabarrett <scottbarrett99@gmail.com>
2024-10-15 01:18:43 -0400

    multi-platform TDM working

--- UVM-NRT-RoS/lora/feather_tdm/include/tdm.h
+++ UVM-NRT-RoS/lora/feather_tdm/include/tdm.h
@@ -1,16 +1,20 @@
 #include <stdint.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 const uint32_t nodes = 4;
-const uint32_t nodeWindowMs = 200;
+const uint32_t nodeWindowMs = 1500;
 const uint32_t frameWindow = nodeWindowMs * 4;
-const uint32_t guardMs = 10;
+const uint32_t guardMs = 500;
 
-bool sendTime();
-bool recvPacket();
+size_t sendTime(uint8_t** buffer = nullptr);
+int recvTime(uint8_t** buffer = nullptr);
+bool isConnected();
 
 // Returns the current node, or -1 if we're in a guard period.
 int32_t currentNode();
+
+constexpr int32_t TIME_NODE = 0;
 
 bool isCurrentNode(uint32_t node);
 uint32_t nowMs();
