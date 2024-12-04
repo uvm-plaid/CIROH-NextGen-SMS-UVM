ef2f29130ae89f130d70af08740871957ac830be
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2024-12-03 23:21:50 -0500

    Updating packet structure to be an approximation of what it will be. Replacing some of the flags fields with more explicit enums. Adding a few utility methods (empty, full) for Lora Server. Turned LoraServer into a singleton. Added default PlatformIO target for the Feather board I am currently using.

---
+++ UVM-NRT-RoS/lora/feather_i2c_server/run_upload.sh
@@ -0,0 +1,16 @@
+# Shell script created for use on MacOS to automatically build the program,
+# find/upload it to a connected Giga, and then monitor it
+
+# Get device path and replace it in platformio.ini
+device_port=$(ls /dev/ttyACM0*)
+
+if [ -z $device_port ]; then
+    echo Could not find connected Feather.
+    exit 1
+else
+    echo Found device in $device_port
+fi
+
+# Build and try to upload
+pio run --target upload
+pio device monitor 
--- UVM-NRT-RoS/lora/feather_i2c_server/src/LoraServer.cpp
+++ UVM-NRT-RoS/lora/feather_i2c_server/src/LoraServer.cpp
@@ -1,29 +1,70 @@
 #include <Arduino.h>
-#include <cstring>
 #include "loraDevice.h"
 
-bool LoraServer::receive(LoraPacket &src) 
+LoraServerStatus LoraServer::receive(LoraPacket &src) 
 {
+    if (full()) {
+        return LoraServerStatus::BUFFER_FULL;
+    }
+
     size_t pointer = writer;
     noInterrupts(); 
     do {
+        Serial.print("Pointer: ");
+        Serial.print(pointer);
+        Serial.print(" Num Packets: ");
+        Serial.println(num_packets);
         pointer = (pointer + 1) % MAX_NUM_PACKETS;
         if (packet_buffer[pointer].flag == LoraPacketStatus::FREE) {
             // Write the packet data into the packet buffer
             packet_buffer[pointer].packet = src;
             packet_buffer[pointer].flag = LoraPacketStatus::FULL;
             writer = pointer; 
+            ++num_packets;
             interrupts(); 
-            return true;
+            return LoraServerStatus::RECEIVE_SUCCESSFUL;
         }
     } while (pointer != writer);
-
     interrupts(); 
-    return false; 
+    return LoraServerStatus::RECEIVE_FAILED; 
 }
 
-bool LoraServer::packetReady() 
+
+LoraServerStatus LoraServer::request(LoraPacket &dst) 
 {
+    if (!find_packet()) {
+        return LoraServerStatus::BUFFER_EMPTY;
+    }
+    Serial.print("Pointer: ");
+    Serial.print(reader);
+    Serial.print(" Num Packets: ");
+    Serial.println(num_packets);
+
+    // Entering critical section
+    noInterrupts();
+    
+    // Check if the packet is marked as FULL
+    if (packet_buffer[reader].flag == LoraPacketStatus::FULL) {
+        // Mark the packet as READING
+        packet_buffer[reader].flag = LoraPacketStatus::READING;
+
+        // Read to destination buffer, then zero out memory
+        dst = packet_buffer[reader].packet;
+        packet_buffer[reader].packet = {};
+        packet_buffer[reader].flag = LoraPacketStatus::FREE;
+        --num_packets;
+        interrupts();
+        return LoraServerStatus::REQUEST_SUCCESSFUL;
+    }
+    
+    interrupts();
+    return LoraServerStatus::REQUEST_FAILED;
+}
+
+bool LoraServer::find_packet() {
+    if (empty()) {
+        return false;
+    }
     // Just in case this got called multiple times and was on a full packet
     if (packet_buffer[reader].flag == LoraPacketStatus::FULL) {
         return true;
@@ -42,29 +83,5 @@ bool LoraServer::packetReady()
 
     // Full rotation around the array, nothing ready yet
     return false;
+
 }
-
-bool LoraServer::request(LoraPacket &dst) 
-{
-    // Check if there's a packet ready and update read pointer if necessary
-    if (packetReady()) {
-        noInterrupts(); // Disable interrupts to enter critical section
-        
-        // Check if the packet is marked as FULL
-        if (packet_buffer[reader].flag == LoraPacketStatus::FULL) {
-            // Mark the packet as READING
-            packet_buffer[reader].flag = LoraPacketStatus::READING;
-
-            // Read to destination buffer, then zero out memory
-            dst = packet_buffer[reader].packet;
-            packet_buffer[reader].packet = {};
-            packet_buffer[reader].flag = LoraPacketStatus::FREE;
-            interrupts();
-            return true;
-        }
-        
-        interrupts();
-    }
-    return false;
-}
-
